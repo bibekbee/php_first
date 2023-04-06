8e76b63b8c03dfac9f34f6fed427e30b4cc9c407
bibekbee <37708553+bibekbee@users.noreply.github.com>
2023-04-06 14:01:30 +0545

    Update form.php

--- form.php
+++ form.php
@@ -1,4 +1,21 @@
 <?php
+
+if ($_SERVER['REQUEST_METHOD'] != 'POST') {
+		header('Location:  ');
+		echo 'Bad output';
+		exit();
+	}
+
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, 'https://ipnpb.sandbox.paypal.com/cgi-bin/webscr');
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, "cmd=_notify-validate&" . http_build_query($_POST));
+	$response = curl_exec($ch);
+	curl_close($ch);
+
 file_put_contents("test.txt" , var_export($_POST = true));
-echo "Hello world!"
+
 ?>
